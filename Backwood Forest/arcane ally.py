5ea31fba643fbe7b7a15074a6506d8e5dba4480e
devssom <lds91@naver.com>
2021-06-11 02:48:09 +0900

    6/11/2021 updated

---
+++ Backwood Forest/arcane ally.py
@@ -0,0 +1,10 @@
+# Take down those ogres! 
+
+while True:
+    enemy = hero.findNearestEnemy()
+    if enemy:
+        if hero.isReady("cleave"):
+            hero.cleave(enemy)
+        else:
+            hero.attack(enemy)
+    pass
