83178e075b1b8ac1d0e2b3dd275ead96235949eb
devssom <78782856+devssom@users.noreply.github.com>
2021-06-11 01:19:48 +0900

    Delete the skeleton.py

--- the skeleton.py
+++
@@ -1,6 +0,0 @@
-# Use a loop to attack the skeleton.
-# Its blunt sword hardly does any damage, but it has huge knockback.
-while True:
-    enemy = hero.findNearestEnemy()
-    if enemy:
-        hero.attack(enemy)
