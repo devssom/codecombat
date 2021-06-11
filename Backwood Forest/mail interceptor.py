df2e4f47a8a8f496141df425070cd424f3054104
devssom <lds91@naver.com>
2021-06-11 23:23:00 +0900

    06/12/2021 updated

---
+++ Backwood Forest/mail interceptor.py
@@ -0,0 +1,13 @@
+# Intercept all ogre messengers from ambush. 
+
+def ambushAttack(target):
+    #  Attack the target if it exists and return to the mark.
+    # Write the function:
+    if target:
+        hero.attack(target)
+        hero.moveXY(52, 36)
+    pass
+
+while True:
+    ogre = hero.findNearestEnemy()
+    ambushAttack(ogre)
